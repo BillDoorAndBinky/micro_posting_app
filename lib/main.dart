33eda17afd4b9762e35770558f7f4561b30b5195
BillDoorAndBinky <DYROGONICHE.doc@gmail.com>
2022-04-05 23:03:45 +0300

    Добавил классы для страницы логина

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'dart:convert' show json, base64, ascii;
+
+import 'HomePage.dart';
+import 'LoginPage.dart';
+
+const SERVER_IP = 'http://192.168.1.167:5000';
+final storage = FlutterSecureStorage();
 
 void main() {
   runApp(const MyApp());
@@ -7,24 +16,44 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  Future<String> get jwtOrEmpty async {
+    var jwt = await storage.read(key: "jwt");
+    if (jwt == null) return "";
+    return jwt;
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FutureBuilder(
+          future: jwtOrEmpty,
+          builder: (context, snapshot) {
+           // if (!snapshot.hasData) return LinearProgressIndicator();
+            if (snapshot.data != "") {
+              var str = snapshot.data.toString();
+              var jwt = str.split(".");
+
+              if (jwt.length != 3) {
+                return LoginPage();
+              } else {
+                var payload = json.decode(
+                    ascii.decode(base64.decode(base64.normalize(jwt[1]))));
+                if (DateTime.fromMillisecondsSinceEpoch(payload["exp"] * 1000)
+                    .isAfter(DateTime.now())) {
+                  return HomePage(str, payload);
+                } else {
+                  return LoginPage();
+                }
+              }
+            } else {
+              return LoginPage();
+            }
+          }),
     );
   }
 }
